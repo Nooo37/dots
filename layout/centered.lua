25320a59822c1992f04dfc3e471b13d5d5d9bb33
Nooo37 <senpai.ackabar@gmail.com>
2020-10-15 15:18:16 +0200

    added support for multiple masters

--- layout/centered.lua
+++ layout/centered.lua
@@ -1,70 +1,78 @@
 local gcolor = require("gears.color")
 local beautiful = require("beautiful")
-local pairs = pairs
+local math = math
 
 local mylayout = {}
 
 mylayout.name = "centered"
+
 function mylayout.arrange(p)
-    local area
-    area = p.workarea
+    local area = p.workarea
     local t = p.tag or screen[p.screen].selected_tag
-    local num_of_clients_in_left_pane  = math.floor((#p.clients - 1)/2)
-    local num_of_clients_in_right_pane = #p.clients - 1 - num_of_clients_in_left_pane
-    local iterable_of_client_on_right_pane = 0
-    local iterable_of_client_on_left_pane  = 0
-    local mwfact = t.master_width_factor 
-    for idx, c in pairs(p.clients) do
-      local g
-      -- Special case: One client
-      if #p.clients == 1 then -- if only one client make fullscreen and leave
+    local mwfact = t.master_width_factor
+    local nmaster = math.min(t.master_count, #p.clients)
+    local nslaves = #p.clients - nmaster
+
+    local master_area_width = area.width * mwfact
+    local slave_area_width  = area.width - master_area_width
+    local master_area_x = area.x + 0.5*slave_area_width
+
+    local number_of_left_sided_slaves =math.floor(nslaves/2)
+    local number_of_right_sided_slaves = nslaves - number_of_left_sided_slaves
+    local left_iterator = 0
+    local right_iterator = 0
+
+    -- Special case: no maters -> rrelapse into awesomes fair layout
+    if t.master_count == 0 then
+        awful.layout.suit.fair.arrange(p)
+        return
+    end
+
+    -- Special case: one slave -> relapse into awesomes masterstack tile layout 
+    if nslaves == 1 then
+        awful.layout.suit.tile.right.arrange(p)
+        return
+    end 
+
+    -- Special case: no slaves -> fullscreen master area
+    if nslaves < 1 then
+        master_area_width = area.width 
+        master_area_x = area.x
+    end 
+    
+    -- iterate through masters
+    for idx=1,nmaster do
+       local c = p.clients[idx]
+       local g
+       g = {
+         x = master_area_x, 
+         y = area.y + (nmaster-idx)*(area.height/nmaster),
+         width = master_area_width,
+         height = area.height/nmaster,
+       }
+       p.geometries[c] = g
+    end
+
+    -- iterate through slaves
+    for idx=1,nslaves do -- idx=nmaster+1,#p.clients do
+      local c = p.clients[idx+nmaster]
+      if idx % 2 == 0 then
         g = {
           x = area.x,
-          y = area.y,
-          width = area.width,
-          height = area.height,
+          y = area.y + left_iterator * (area.height/number_of_left_sided_slaves),
+          width = slave_area_width/2,
+          height = area.height/number_of_left_sided_slaves,
         }
-      -- Special case: Two clients
-      elseif idx == 1 and #p.clients == 2 then -- if client is master and there are two clients in total
+        left_iterator = left_iterator + 1
+      else
         g = {
-          x = area.x,
-          y = area.y,
-          width = area.width * mwfact,
-          height = area.height,
+          x = area.x + master_area_width + slave_area_width/2,
+          y = area.y + right_iterator * (area.height/number_of_right_sided_slaves),
+          width = slave_area_width/2,
+          height = area.height/number_of_right_sided_slaves,
         }
-      elseif idx == 2 and #p.clients == 2 then -- if client is slave and there are two clients in total
-          g = {
-            x = area.x + area.width * mwfact,
-            y = area.y,
-            width = area.width * (1-mwfact),
-            height = area.height,
-          }
-      -- Normal case: More than 2 clients
-      elseif idx == 1 then -- if client is master (and there more than 2 clients in total)
-        g = {
-          x = area.x + (1-mwfact)/2 * area.width,
-          y = area.y,
-          width = area.width * mwfact,
-          height = area.height,
-        }
-      else -- if client is slave
-        if idx % 2 == 0 then -- if client on right pane
-          g = {
-            x = area.x + mwfact * area.width + (1-mwfact)/2 * area.width,
-            y = area.y + iterable_of_client_on_right_pane * (area.height/num_of_clients_in_right_pane),
-            width = (1-mwfact)/2 * area.width,
-            height = area.height/num_of_clients_in_right_pane,
-          }
-          iterable_of_client_on_right_pane = iterable_of_client_on_right_pane + 1
-        else -- if client on left pane
-          g = {
-            x = area.x,
-            y = area.y + iterable_of_client_on_left_pane * (area.height/num_of_clients_in_left_pane),
-            width = (1-mwfact)/2 * area.width,
-            height = area.height/num_of_clients_in_left_pane,
-          }
-          iterable_of_client_on_left_pane = iterable_of_client_on_left_pane + 1
-        end
+        right_iterator = right_iterator + 1
+
       end
       p.geometries[c] = g
     end
