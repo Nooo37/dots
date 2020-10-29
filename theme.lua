cf7eb5c01fb8d8ea4983c295153be1e9ef1027dd
Nooo37 <senpai.ackabar@gmail.com>
2020-10-29 20:47:10 +0100

    inherit icons from default theme

--- theme.lua
+++ theme.lua
@@ -11,11 +11,12 @@ local helpers = require("ui.helpers") -- only for notification appearance
 local config_path = os.getenv("HOME") .. "/.config/awesome/"
 
 
-local theme = {}
+-- inherent from default file (only for the icons)
+local theme = dofile("/usr/share/awesome/themes/default/theme.lua")
 
 theme.weather_city = "Berlin"
 
-theme.terminal = "alacritty"
+theme.terminal = "Alacritty"
 theme.browser  = "Firefox"
 theme.editor   = "Atom"
 
@@ -23,69 +24,15 @@ theme.config_path = config_path
 theme.wallpaper = config_path.."wallpaper.png"
 
 theme.corner_radius = dpi(8) --14   8
+theme.border_radius = dpi(8) --14   8
 theme.font          = "sans 8"
 
+
+
 -- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
 theme.menu_submenu_icon = config_path.."default/submenu.png" --TODO wtf
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
-
--- Define the image to load
-theme.titlebar_close_button_normal              = config_path.."icon/titlebar/close_normal.png"
-theme.titlebar_close_button_focus               = config_path.."icon/titlebar/close_focus.png"
-
-theme.titlebar_minimize_button_normal           = config_path.."icon/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus            = config_path.."icon/titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive     = config_path.."icon/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive      = config_path.."icon/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active       = config_path.."icon/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active        = config_path.."icon/titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive    = config_path.."icon/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive     = config_path.."icon/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active      = config_path.."icon/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active       = config_path.."icon/titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive  = config_path.."icon/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive   = config_path.."icon/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active    = config_path.."icon/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active     = config_path.."icon/titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = config_path.."icon/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = config_path.."icon/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active   = config_path.."icon/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active    = config_path.."icon/titlebar/maximized_focus_active.png"
-
-
--- You can use your own layout icons like this:
-theme.layout_fairh      = config_path.."icon/layouts/fairhw.png"
-theme.layout_fairv      = config_path.."icon/layouts/fairvw.png"
-theme.layout_floating   = config_path.."icon/layouts/floatingw.png"
-theme.layout_magnifier  = config_path.."icon/layouts/magnifierw.png"
-theme.layout_max        = config_path.."icon/layouts/maxw.png"
-theme.layout_fullscreen = config_path.."icon/layouts/fullscreenw.png"
-theme.layout_tilebottom = config_path.."icon/layouts/tilebottomw.png"
-theme.layout_tileleft   = config_path.."icon/layouts/tileleftw.png"
-theme.layout_tile       = config_path.."icon/layouts/tilew.png"
-theme.layout_tiletop    = config_path.."icon/layouts/tiletopw.png"
-theme.layout_spiral     = config_path.."icon/layouts/spiralw.png"
-theme.layout_dwindle    = config_path.."icon/layouts/dwindlew.png"
-theme.layout_cornernw   = config_path.."icon/layouts/cornernww.png"
-theme.layout_cornerne   = config_path.."icon/layouts/cornernew.png"
-theme.layout_cornersw   = config_path.."icon/layouts/cornersww.png"
-theme.layout_cornerse   = config_path.."icons/layouts/cornersew.png"
-
-theme.icon = {
-  main = config_path.."icon/tags/main.png",
-  web  = config_path.."icon/tags/web.png",
-  code = config_path.."icon/tags/code.png",
-  read = config_path.."icon/tags/read.png",
-  chat = config_path.."icon/tags/chat.png",
-}
-
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
@@ -135,7 +82,7 @@ theme.gap_single_client = false
 theme.useless_gap   = dpi(5)
 
 -- borders
-theme.border_width  = dpi(2)
+theme.border_width  = dpi(0)
 theme.border_normal = theme.xcolor0
 theme.border_focus  = theme.xcolor1
 theme.border_marked = theme.xcolor10
@@ -157,6 +104,26 @@ theme.taglist_fg_empty                          = theme.xcolor8
 theme.taglist_bg_volatile                       = "#00000000"
 theme.taglist_fg_volatile                       = theme.xcolor15
 
+local taglist_square_size = dpi(0)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+                                taglist_square_size, theme.fg_normal)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+                                  taglist_square_size, theme.fg_normal)
+
+
+-- tasklist
+theme.tasklist_bg                                = "alpha"
+theme.tasklist_bg_focus                          = theme.xcolor2
+theme.tasklist_fg_focus                          = theme.xfg
+theme.tasklist_bg_urgent                         = theme.xcolor2
+theme.tasklist_fg_urgent                         = theme.xfg
+theme.tasklist_bg_occupied                       = "alpha"
+theme.tasklist_fg_occupied                       = theme.xfg
+theme.tasklist_bg_normal                         = "alpha"
+theme.tasklist_fg_normal                         = theme.xcolor8
+theme.tasklist_bg_volatile                       = "#00000000"
+theme.tasklist_fg_volatile                       = theme.xcolor15
+
 -- titlebar
 theme.titlebar_fg_normal 	                      = theme.xfg
 theme.titlebar_bg_normal 	                      = theme.xbg
@@ -172,55 +139,17 @@ theme.menu_submenu_icon = config_path.."default/submenu.png"
 theme.menu_height = dpi(16)
 theme.menu_width  = dpi(100)
 
+theme.hotkeys_group_margin = dpi(40)
+theme.hotkeys_shape = helpers.rrect(theme.border_radius)
+theme.hotkeys_border_color = theme.xcolor1
+theme.hotkeys_border_width = theme.border_width
+theme.hotkeys_modifiers_fg = theme.xcolor8
+theme.hotkeys_label_fg = theme.xbg
+theme.hotkeys_label_bg = theme.xcolor5
+
+
 -- Recolor Layout icons:
 theme = theme_assets.recolor_layout(theme, theme.fg_normal)
-
--- Recolor titlebar icons:
---
--- local function darker(color_value, darker_n)
---     local result = "#"
---     for s in color_value:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
---         local bg_numeric_value = tonumber("0x"..s) - darker_n
---         if bg_numeric_value < 0 then bg_numeric_value = 0 end
---         if bg_numeric_value > 255 then bg_numeric_value = 255 end
---         result = result .. string.format("%2.2x", bg_numeric_value)
---     end
---     return result
--- end
--- theme = theme_assets.recolor_titlebar(
---     theme, theme.fg_normal, "normal"
--- )
--- theme = theme_assets.recolor_titlebar(
---     theme, darker(theme.fg_normal, -60), "normal", "hover"
--- )
--- theme = theme_assets.recolor_titlebar(
---     theme, xrdb.color1, "normal", "press"
--- )
--- theme = theme_assets.recolor_titlebar(
---     theme, theme.fg_focus, "focus"
--- )
--- theme = theme_assets.recolor_titlebar(
---     theme, darker(theme.fg_focus, -60), "focus", "hover"
--- )
--- theme = theme_assets.recolor_titlebar(
---     theme, xrdb.color1, "focus", "press"
--- )
-
--- Define the icon theme for application icons. If not set then the icons
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil
-
--- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(
-    theme.menu_height, theme.bg_focus, theme.fg_focus
-)
-
--- Try to determine if we are running light or dark colorscheme:
-local bg_numberic_value = 0;
-for s in theme.bg_normal:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
-    bg_numberic_value = bg_numberic_value + tonumber("0x"..s);
-end
-local is_dark_bg = (bg_numberic_value < 383)
 
 -- Notifications appearance
 naughty.config.defaults['icon_size']  = 50
@@ -266,13 +195,21 @@ theme.collision_focus_bg            = theme.xcolor8
 theme.collision_focus_fg            = theme.xbg
 theme.collision_focus_bg_center     = theme.xcolor8
 -- theme.collision_screen_shape        =
-theme.collision_screen_border_width = 0 
+theme.collision_screen_border_width = 0
 theme.collision_screen_border_color = theme.xcolor5
 -- theme.collision_screen_padding      =
 theme.collision_screen_bg           = theme.xcolor8
 theme.collision_screen_fg           = theme.xcolor2
 theme.collision_screen_bg_focus     = theme.xcolor8
 theme.collision_screen_fg_focus     = theme.xcolor1
+
+
+-- bling
+theme.mstab_tabbar_orientation = "bottom"
+theme.mstab_bar_height = 35
+theme.mstab_font = "sans 10"
+theme.flash_focus_start_opacity = 0.8
+theme.flash_focus_step = 0.05
 
 
 naughty.config.shape = helpers.rrect(5)
